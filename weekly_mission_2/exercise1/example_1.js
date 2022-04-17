c784bb186e146f5b7a69b84cd1373efbca5194e9
Jose <jluiscancino115@gmail.com>
2022-04-17 16:44:09 -0500

    actualizacion-ejercicios_1

--- weekly_mission_2/exercise1/example_1.js
+++ weekly_mission_2/exercise1/example_1.js
@@ -19,3 +19,37 @@ const twitter = {
 console.log("Nombre de usuario: " + twitter.username)
 console.log("Total de interacciones: " + twitter.totalInteractions())
 console.log(twitter.information()+"\n")
+
+/* Ejemplo 2 */
+console.log("Ejemplo Facebook");
+const facebook = {
+  user: "Pedri Gonzalez",
+  ciudad_actual: "Barcelona",
+  centro_edu: "Instituto Las Palmas",
+  localidad: "Tegueste, Tenerife",
+  detalles: function () {
+    return `Detalles sobre ${this.user}\nVive en: ${this.ciudad_actual}\nSe formo en: ${this.centro_edu}\nEs de la ciudad de: ${this.localidad}`
+  }
+}
+
+console.log(facebook.detalles()+"\n")
+
+/* Ejemplo 3 */
+console.log("Ejemplo Instagram");
+const instagram = {
+  user: "Pablo Sarabia",
+  username: "pablosarabia17",
+  posts: 23,
+  seguidos: 120,
+  seguidores: 90,
+  publicaciones: function() {
+    return `${this.username} ha realizado ${this.posts} publicaciones`
+  },
+  information: function () {
+    return `${this.user} tiene ${this.seguidores} seguidores`
+  }
+}
+
+console.log("Nombre de usuario: " + instagram.username)
+console.log("Publicaciones realizdas: " + instagram.publicaciones())
+console.log(instagram.information())
